6bb9f6f439e85c59bbcb41c856952e2ce5be6834
antropova <masha.antropova@gmail.com>
2015-09-07 18:19:35 -0400

    Add provider column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150904235547) do
+ActiveRecord::Schema.define(version: 20150907221555) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150904235547) do
     t.text     "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "provider"
   end
 
 end
